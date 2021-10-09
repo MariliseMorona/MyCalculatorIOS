813fcce650cb64d714c2a3d1c2310fdc3e62d48d
Marilise Morona <marymorona@gmail.com>
2021-10-09 03:49:58 -0300

    Reference IBOutlet

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlet
+    // Outlet faz referência a parte visual
+   
+    
+    @IBOutlet weak var uibAC: UIButton!
+    @IBOutlet weak var uibSinal: UIButton!
+    @IBOutlet weak var uibPercent: UIButton!
+    @IBOutlet weak var uibDivisor: UIButton!
+    @IBOutlet weak var uibMultiplicator: UIButton!
+    @IBOutlet weak var uibSubtractor: UIButton!
+    @IBOutlet weak var uibSum: UIButton!
+    @IBOutlet weak var uibEquals: UIButton!
+    @IBOutlet weak var uibComma: UIButton!
+    @IBOutlet weak var uibNine: UIButton!
+    @IBOutlet weak var uibEight: UIButton!
+    @IBOutlet weak var uibSeven: UIButton!
+    @IBOutlet weak var uibSix: UIButton!
+    @IBOutlet weak var uibFive: UIButton!
+    @IBOutlet weak var uibFour: UIButton!
+    @IBOutlet weak var uibThree: UIButton!
+    @IBOutlet weak var uibTwo: UIButton!
+    @IBOutlet weak var uibOne: UIButton!
+    @IBOutlet weak var uibZero: UIButton!
+    @IBOutlet weak var uitfVisor: UITextField!
+    
+    
+    
+    // MARK: - Variáveis
+    
+    
+    
+    // MARK: - Ciclo de Vida (life cycle)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+       
+         
+      
+         }
+         
+    // MARK: - Metodos
+    
+    
+    // MARK: - IBAction
+    // Action faz referência a ação
+    
 }
+
+
 
